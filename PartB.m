839db25fcd2903da412ed34a77fc3f6f5a836af5
Razi Murshed <razi.murshed@mail.mcgill.ca>
2016-11-09 15:20:02 -0500

    Update report

--- PartB.m
+++ PartB.m
@@ -1,4 +1,6 @@
 Tw = Data1.Tw;
+T0 = Data1.T0;
+T19 = Data1.T19;
 [clusteridxSpike] = kmeans(Tw, 4);
 
 unit4Ts1 =[];
@@ -22,6 +24,101 @@ unit4Ts1 = unit4Ts1';
 unit3Ts1 = transpose(unit3Ts1);
 unit2Ts1 = transpose(unit2Ts1);
 unit1Ts1 = transpose(unit1Ts1);
+%spkTrials1 
+for i = 1:length(T0)
+  spkInTrial = [];
+  tsByTrial =[];
+  for j = 1:length(unit1Ts1)
+    if (unit1Ts1(j) > T0(i) && unit1Ts1(j) < T19(i))
+      spkInTrial = [spkInTrial, j]; 
+    end
+  end
+  spkInTrial = spkInTrial';
+  for j = 1:length(spkInTrial)
+      tsByTrial = [tsByTrial,unit1Ts1(spkInTrial(j))]; 
+  end
+  tsByTrial =tsByTrial';
+  if ~isempty(tsByTrial)
+  spkTrials1(i, 1:length(spkInTrial)) = tsByTrial - tsByTrial(1);
+  end
+end
+
+%spkTrials2 
+for i = 1:length(T0)
+  spkInTrial = [];
+  tsByTrial =[];
+  for j = 1:length(unit2Ts1)
+    if (unit2Ts1(j) > T0(i) && unit2Ts1(j) < T19(i))
+      spkInTrial = [spkInTrial, j]; 
+    end
+  end
+  spkInTrial = spkInTrial';
+  for j = 1:length(spkInTrial)
+      tsByTrial = [tsByTrial,unit2Ts1(spkInTrial(j))]; 
+  end
+  tsByTrial =tsByTrial';
+  if ~isempty(tsByTrial)
+  spkTrials2(i, 1:length(spkInTrial)) = tsByTrial - tsByTrial(1);
+  end
+end
+
+%spkTrials3 
+for i = 1:length(T0)
+  spkInTrial = [];
+  tsByTrial =[];
+  for j = 1:length(unit3Ts1)
+    if (unit3Ts1(j) > T0(i) && unit3Ts1(j) < T19(i))
+      spkInTrial = [spkInTrial, j]; 
+    end
+  end
+  spkInTrial = spkInTrial';
+  for j = 1:length(spkInTrial)
+      tsByTrial = [tsByTrial,unit3Ts1(spkInTrial(j))]; 
+  end
+  tsByTrial =tsByTrial';
+  if ~isempty(tsByTrial)
+  spkTrials3(i, 1:length(spkInTrial)) = tsByTrial - tsByTrial(1);
+  end
+end
+
+%spkTrials4 
+for i = 1:length(T0)
+  spkInTrial = [];
+  tsByTrial =[];
+  for j = 1:length(unit3Ts1)
+    if (unit4Ts1(j) > T0(i) && unit4Ts1(j) < T19(i))
+      spkInTrial = [spkInTrial, j]; 
+    end
+  end
+  spkInTrial = spkInTrial';
+  for j = 1:length(spkInTrial)
+      tsByTrial = [tsByTrial,unit4Ts1(spkInTrial(j))]; 
+  end
+  tsByTrial =tsByTrial';
+  if ~isempty(tsByTrial)
+  spkTrials4(i, 1:length(spkInTrial)) = tsByTrial - tsByTrial(1);
+  end
+end
+
+%spkTrialsUnsorted 
+for i = 1:length(T0)
+  spkInTrial = [];
+  tsByTrial =[];
+  for j = 1:length(Tw)
+    if (Tw(j) > T0(i) && Tw(j) < T19(i))
+      spkInTrial = [spkInTrial, j]; 
+    end
+  end
+  spkInTrial = spkInTrial';
+  for j = 1:length(spkInTrial)
+      tsByTrial = [tsByTrial,Tw(spkInTrial(j))]; 
+  end
+  tsByTrial =tsByTrial';
+  if ~isempty(tsByTrial)
+  spkTrialsU(i, 1:length(spkInTrial)) = tsByTrial - tsByTrial(1);
+  end
+end
+
 
 Tmem1 = Data1.Tmem1;
 Tmem2 = Data1.Tmem2;
@@ -117,21 +214,29 @@ SEU = SEU';
 FRMemU = FRMemU';
 
 
-figure
-errorbar(FRMem1, SE1);
-figure
-errorbar(FRMem2, SE2);
-figure
-errorbar(FRMem3, SE3);
-figure
-errorbar(FRMem4, SE4);
-figure
-errorbar(FRMemU, SEU);
+% figure
+% errorbar(FRMem1, SE1);
+% figure
+% errorbar(FRMem2, SE2);
+% figure
+% errorbar(FRMem3, SE3);
+% figure
+% errorbar(FRMem4, SE4);
+% figure
+% errorbar(FRMemU, SEU);
 
-[p,tbl,stats] = anova1(FRMem1,Data1.Hang, 'off');
-[p,tbl,stats] = anova1(FRMem2,Data1.Hang, 'off');
-[p,tbl,stats] = anova1(FRMem3,Data1.Hang, 'off');
-[p,tbl,stats] = anova1(FRMem4,Data1.Hang, 'off');
-[p,tbl,stats] = anova1(FRMemU,Data1.Hang, 'off');
-
-multcompare(stats);
+[p,tbl,stats1] = anova1(FRMem1,Data1.Hang, 'off');
+figure
+multcompare(stats1);
+[p,tbl,stats2] = anova1(FRMem2,Data1.Hang, 'off');
+figure
+multcompare(stats2);
+[p,tbl,stats3] = anova1(FRMem3,Data1.Hang, 'off');
+figure
+multcompare(stats3);
+[p,tbl,stats4] = anova1(FRMem4,Data1.Hang, 'off');
+figure
+multcompare(stats4);
+[p,tbl,statsu] = anova1(FRMemU,Data1.Hang, 'off');
+figure
+multcompare(statsu);
